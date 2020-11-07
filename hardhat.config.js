b4653f812a21188ab75e8f05ed7143b382c5a87a
libo <libo@allinmd.cn>
2020-11-07 19:38:50 +0800

    updata

---
+++ hardhat.config.js
@@ -0,0 +1,55 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async taskArgs => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
+  
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/95dbd9a5623a427e971285c3cfd87a7d",
+      accounts: ['8ba87cdc753452ea2f95f89604f62dfb678fcf845c5f89f3db77589edaa7090b']
+    }
+  },
+  solidity: {
+    version: "0.5.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
+
